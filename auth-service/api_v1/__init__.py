ee70d26467b984fc37fb48f8c20fc2bd56607578
Doonu <DonuSuzerain@yandex.ru>
2025-02-28 09:09:06 +0300

    feat: добавлены зависимости

--- auth-service/api_v1/__init__.py
+++ auth-service/api_v1/__init__.py
@@ -5,6 +5,6 @@ from .session import session_router
 from .auth import auth_router
 
 router = APIRouter()
-router.include_router(user_router, prefix="/user", tags=["user"])
-router.include_router(session_router, prefix="/session", tags=["session"])
 router.include_router(auth_router, prefix="/auth", tags=["auth"])
+router.include_router(session_router, prefix="/session", tags=["session"])
+router.include_router(user_router, prefix="/user", tags=["user"])
